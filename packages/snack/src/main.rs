85fc6cdc25c914aa501e9eab9aff1db38008be1c
wangcheng <wangcheng@deepblueai.com>
2020-10-16 18:05:38 +0800

    feat: condition

--- packages/snack/src/main.rs
+++ packages/snack/src/main.rs
@@ -6,8 +6,17 @@ fn main() {
         x + 1
     };
     println!("{} {}", y, result);
+    condition_function(y);
 }
 
 fn another_function(x: i32) -> i32 {
     x
+}
+
+fn condition_function(number: i32) {
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
 }
