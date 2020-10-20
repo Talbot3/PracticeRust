de1784e207a887c6f42d223e8c38045903afe9fc
wangcheng <wangcheng@deepblueai.com>
2020-10-20 18:22:12 +0800

    docs: format

--- packages/rtsp-mock/src/main.rs
+++ packages/rtsp-mock/src/main.rs
@@ -6,53 +6,49 @@ use {
 };
 
 fn main() {
-
     // 读取参数
     let args: Vec<String> =env::args().collect();
+    let mut count: i32 = match args[1].trim().parse() {
+        Ok(num) => num,
+        Err(error) => panic!("请需要rtsp流的数量: {:?}", error),
+    };
+
+    gstreamer::init().unwrap();
+    let media_factory = RTSPMediaFactory::new();
+    media_factory.set_launch("videotestsrc ! clockoverlay halignment=left valignment=top text=\"by 司马错 with deeplueai.com  \" shaded-background=true font-desc=\"Sans, 36\"  ! x264enc key-int-max=40 ! rtph264pay name=pay0 pt=96");
+    media_factory.set_shared(true);
+
+    let main_context = MainContext::default();
+    let server = RTSPServer::new();
+    let source_id = server.attach(Some(&main_context));
+    let mount_points = server.get_mount_points().unwrap();
+
+    // 数组
+    let mut end_points: Vec<String> = Vec::new();
+    let mut index = 0;
+    while count != 0 {
+        let mut end_point = String::from("/");
+        let count_str = count.to_string();
+        end_point.push_str(&count_str);
+        end_points.push(end_point);
+        mount_points.add_factory(&end_points[index], &media_factory);
+        count -= 1;
+        index += 1;
+    }
+
+    let main_loop = MainLoop::new(Some(&main_context), false);
+
+    let address = server.get_address().unwrap();
+    let port = server.get_bound_port();
     
-        println!("请需要需要模拟的rtsp流的数量 $: ");
-        println!("{:?}", args);
-        let mut count: i32 = match args[1].trim().parse() {
-            Ok(num) => num,
-            Err(error) => panic!("Problem Invalidate Int: {:?}", error),
-        };
-
-        gstreamer::init().unwrap();
-        let media_factory = RTSPMediaFactory::new();
-        media_factory.set_launch("videotestsrc ! clockoverlay halignment=left valignment=top text=\"by 司马错 with deeplueai.com  \" shaded-background=true font-desc=\"Sans, 36\"  ! x264enc key-int-max=40 ! rtph264pay name=pay0 pt=96");
-        media_factory.set_shared(true);
-
-        let main_context = MainContext::default();
-        let server = RTSPServer::new();
-        let source_id = server.attach(Some(&main_context));
-        let mount_points = server.get_mount_points().unwrap();
-
-        // 数组
-        let mut end_points: Vec<String> = Vec::new();
-        let mut index = 0;
-        while count != 0 {
-            let mut end_point = String::from("/");
-            let count_str = count.to_string();
-            end_point.push_str(&count_str);
-            end_points.push(end_point);
-            mount_points.add_factory(&end_points[index], &media_factory);
-            count -= 1;
-            index += 1;
-        }
-
-        let main_loop = MainLoop::new(Some(&main_context), false);
-
-        let address = server.get_address().unwrap();
-        let port = server.get_bound_port();
-        
-        // 迭代输出rtsp模拟流
-        let path_iter = end_points.iter();
-        println!("server running");
-        for path in path_iter {
-            let location = format!("rtsp://{}:{}{}", address, port, path);
-            println!("{}", location);
-        }
-        
-        main_loop.run();
-        glib::source_remove(source_id);
+    // 迭代输出rtsp模拟流
+    let path_iter = end_points.iter();
+    println!("server running");
+    for path in path_iter {
+        let location = format!("rtsp://{}:{}{}", address, port, path);
+        println!("{}", location);
+    }
+    
+    main_loop.run();
+    glib::source_remove(source_id);
 }
