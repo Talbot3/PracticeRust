afe3e95c2ec51953e2a8ab80e1e9bc7b80a3e16c
wangcheng <wangcheng@deepblueai.com>
2020-12-20 12:35:10 +0800

    fix: params

--- packages/rtsp-mock/src/main.rs
+++ packages/rtsp-mock/src/main.rs
@@ -8,6 +8,9 @@ use {
 fn main() {
     // 读取参数
     let args: Vec<String> =env::args().collect();
+    if  args.len()<=1 {
+        return println!("请输入rtsp模拟器数量");
+    }
     let mut count: i32 = match args[1].trim().parse() {
         Ok(num) => num,
         Err(error) => panic!("请需要rtsp流的数量: {:?}", error),
@@ -23,7 +26,7 @@ fn main() {
     let source_id = server.attach(Some(&main_context));
     let mount_points = server.get_mount_points().unwrap();
 
-    // 数组
+    // // 数组
     let mut end_points: Vec<String> = Vec::new();
     let mut index = 0;
     while count != 0 {
@@ -50,5 +53,6 @@ fn main() {
     }
     
     main_loop.run();
+    println!("{:?}", source_id);
     glib::source_remove(source_id);
 }
